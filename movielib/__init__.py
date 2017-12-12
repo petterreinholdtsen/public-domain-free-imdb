2ddb8101fac18cf0c0f8c9908d250ff9d745c23e
Petter Reinholdtsen <pere@hungry.com>
2017-12-12 08:49:32 +0100

    Move test code to library.

--- movielib/__init__.py
+++ movielib/__init__.py
@@ -13,8 +13,24 @@ def http_get_read(url):
             f = opener.open(url)
             return f.read()
         except:
-            print("Retrying %s in 20 seconds" % url)
-            time.sleep(20)
+            print("Retrying %s in 2 seconds" % url)
+            time.sleep(2)
+
+def test_wikipedia_lookup():
+    for line in [
+            '* {{IMDb title|0005339|A Fool There Was}}',
+            '*{{IMDb title|id=0013704|title=The Trap}}',
+            '*{{IMDb title|id=0036697|title=Captain America}}',
+            '* {{IMDb title|id=0052646}}',
+            '* {{IMDb title|id =0175627|title =La fuga degli amanti  }}'
+    ]:
+        m = re.search("\* ?{{IMDb title *\| *(id *=)?(tt)?(\d+) *(\|?.+}})?",
+                      line, re.IGNORECASE)
+        if m is not None:
+            imdburl = 'http://www.imdb.com/title/tt%s/' % m.group(3)
+            print imdburl
+        else:
+            print("Error %s" % line)
 
 def wikipedia_lookup(wpurl):
     m = re.search('^(.*wikipedia.org)/wiki/(.+)', wpurl, re.IGNORECASE)
@@ -24,7 +40,7 @@ def wikipedia_lookup(wpurl):
     for line in text.split("\n"):
         #print line
         if -1 != line.lower().find('{{imdb title'):
-            m = re.search("\* ?{{IMDb title *\| *(id=)?(tt)?(\d+) *(\|?.+}})?",
+            m = re.search("\* ?{{IMDb title *\| *(id *=)?(tt)?(\d+) *(\|?.+}})?",
                           line, re.IGNORECASE)
             if m:
                 # Normalize URLs to 7 digit numbers, as some wikipedia
@@ -35,3 +51,5 @@ def wikipedia_lookup(wpurl):
                 print("info: '%s' ignored in %s" % (line, wpurl))
     return None
 
+if __name__ == '__main__':
+    test_wikipedia_lookup()
